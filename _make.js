9b84cbd9ee56385cbc27c6f9a145266b6658fc6e
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-03-28 17:23:34 +0800

    feat: update load code

--- _make.js
+++ _make.js
@@ -1,5 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const SOURCE_DIR = path.join(__dirname, "..", "ofa.js", "tutorial", "website");
 const TARGET_DIR = __dirname;
